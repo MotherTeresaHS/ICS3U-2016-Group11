7108c2af7caa84d1c562f45cdcea19f5974f2d2f
Patrick Coxall <mr.coxall@mths.ca>
2016-12-08 13:12:57 -0500

    Iniy

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# ICS3U-2016-Group11
+# ICS3U-2016-Group1
+
+Coders: 
+- Boulay, Kyle
+- Anglin, Tristan
+- Dory, Justin
---
+++ main_menu_scene.py
@@ -0,0 +1,50 @@
+# Created by: Mr. Coxall
+# Created on: Sep 2016
+# Created for: ICS3U
+# This scene shows the main menu.
+
+from scene import *
+import ui
+
+
+class MainMenuScene(Scene):
+    def setup(self):
+        # this method is called, when user moves to this scene
+        
+        # add MT blue background color
+        self.background = SpriteNode(position = self.size / 2, 
+                                     color = 'white', 
+                                     parent = self, 
+                                     size = self.size)
+    
+    def update(self):
+        # this method is called, hopefully, 60 times a second
+        pass
+    
+    def touch_began(self, touch):
+        # this method is called, when user touches the screen
+        pass
+    
+    def touch_moved(self, touch):
+        # this method is called, when user moves a finger around on the screen
+        pass
+    
+    def touch_ended(self, touch):
+        # this method is called, when user releases a finger from the screen
+        pass
+    
+    def did_change_size(self):
+        # this method is called, when user changes the orientation of the screen
+        # thus changing the size of each dimension
+        pass
+    
+    def pause(self):
+        # this method is called, when user touches the home button
+        # save anything before app is put to background
+        pass
+    
+    def resume(self):
+        # this method is called, when user place app from background 
+        # back into use. Reload anything you might need.
+        pass
+    
